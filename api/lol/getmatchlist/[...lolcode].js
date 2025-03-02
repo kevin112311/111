ef98f87226b7f78d2954820085b308f7b034fb9f
kevin <kevin2524h@qq.com>
2025-03-02 22:49:43 +0800

    13 commit

--- api/lol/getmatchlist/[...lolcode].js
+++ api/lol/getmatchlist/[...lolcode].js
@@ -5,41 +5,44 @@ export default async function handler(req, res) {
     const fullUrl = `http://${req.headers.host}${req.url}`;
     const urlObj = new URL(fullUrl);
     const pathname = urlObj.pathname;
+    console.log(pathname);
     const parts = pathname.split('/');
+    console.log(parts);
     const lolcode = parts.slice(4);
+    console.log(lolcode);
     const firstPart = lolcode[0];
-    const secondPart = lolcode[1];
+    var mess = lolcode[1] + '/ids?';
+    if (lolcode.length >= 3) {
+        mess += lolcode[2] + '&';
+    }
     if (regin.includes(firstPart)) {
         if (firstPart == 'JP') {
             try {
-                const response = await axios.get('https://jp1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + secondPart + '?api_key=' + key, {
+                const response = await axios.get('https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/' + mess + 'api_key=' + key, {
                     timeout: 5000  // 设置请求超时为 5 秒
                 });
                 res.json(response.data);
             } catch (error) {
-                // 处理错误
                 res.status(500).send('Something went wrong with the API request');
             }
         }
         else if (firstPart == 'KR') {
             try {
-                const response = await axios.get('https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/' + secondPart + '?api_key=' + key, {
+                const response = await axios.get('https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/' + mess + 'api_key=' + key, {
                     timeout: 5000  // 设置请求超时为 5 秒
                 });
                 res.json(response.data);
             } catch (error) {
-                // 处理错误
                 res.status(500).send('Something went wrong with the API request');
             }
         }
         else if (firstPart == 'EU') {
             try {
-                const response = await axios.get('https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + secondPart + '?api_key=' + key, {
+                const response = await axios.get('https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/' + mess + 'api_key=' + key, {
                     timeout: 5000  // 设置请求超时为 5 秒
                 });
                 res.json(response.data);
             } catch (error) {
-                // 处理错误
                 res.status(500).send('Something went wrong with the API request');
             }
         }
